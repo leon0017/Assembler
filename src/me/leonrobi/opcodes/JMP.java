4ee4259483c22a7440263aba25d023c1a96a5571
leon-robinson <leonrobinson.dev@gmail.com>
2022-09-24 16:50:10 +0100

    More JMP opcodes.

--- src/me/leonrobi/opcodes/JMP.java
+++ src/me/leonrobi/opcodes/JMP.java
@@ -11,29 +11,57 @@ import java.util.List;
 public class JMP extends Opcode {
 
 	private enum JMPType {
-		JMP,
-		JNE,
-		JE
-	}
+		JMP((byte)0xeb),
+		JO((byte)0x70),
+		JNO((byte)0x71),
+		JS((byte)0x78),
+		JNS((byte)0x79),
+		JE((byte)0x74),
+		JZ((byte)0x74),
+		JNE((byte)0x75),
+		JNZ((byte)0x75),
+		JB((byte)0x72),
+		JNAE((byte)0x72),
+		JC((byte)0x72),
+		JNB((byte)0x73),
+		JAE((byte)0x73),
+		JNC((byte)0x73),
+		JBE((byte)0x76),
+		JNA((byte)0x76),
+		JA((byte)0x77),
+		JNBE((byte)0x77),
+		JL((byte)0x7c),
+		JNGE((byte)0x7c),
+		JGE((byte)0x7d),
+		JNL((byte)0x7d),
+		JLE((byte)0x7e),
+		JNG((byte)0x7e),
+		JG((byte)0x7f),
+		JNLE((byte)0x7f),
+		JP((byte)0x7a),
+		JPE((byte)0x7a),
+		JNP((byte)0x7b),
+		JPO((byte)0x7b),
+		JCXZ((byte)0xe3);
 
-	private byte getShortJmpByte(JMPType jmpType) {
-		switch (jmpType) {
-			case JMP -> {
-				return (byte)0xfe;
-			}
-			case JNE -> {
-				return (byte)0x75;
-			}
-			case JE -> {
-				return (byte)0x74;
-			}
+		private final byte shortJumpOpcode;
+
+		JMPType(byte shortJumpOpcode) {
+			this.shortJumpOpcode = shortJumpOpcode;
 		}
-		return (byte)0x0;
+
+		public byte getShortJumpOpcode() {
+			return this.shortJumpOpcode;
+		}
 	}
 
 	@Override
 	public String[] identifiers() {
-		return new String[]{"jmp","jne","je"};
+		JMPType[] values = JMPType.values();
+		String[] identifiers = new String[values.length];
+		for (int i = 0; i < identifiers.length; i++)
+			identifiers[i] = values[i].toString().toLowerCase();
+		return identifiers;
 	}
 
 	@Override
@@ -55,9 +83,9 @@ public class JMP extends Opcode {
 
 		List<Byte> bytes = new ArrayList<>();
 
-		if (offset <= 127 && offset >= -127) { // Small Jump
+		if (offset <= 127 && offset >= -127) { // Short Jump
 			byte offsetByte = (byte) (offset & 0xff);
-			bytes.add(getShortJmpByte(jmpType));
+			bytes.add(jmpType.getShortJumpOpcode());
 			if (offset <= 0) // Backwards OR same location
 				bytes.add((byte)((0xff + offsetByte) - 0x1));
 			else
